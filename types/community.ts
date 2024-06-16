0f36718fe1aec9bfc4feafb3f1164bfebe039ec1
Anniel-Francisco <annielfranciscoreyes@gmail.com>
2024-06-16 01:06:10 -0400

    style: cambiando nombre de type communities por community y agregando nuevas propiedades

--- types/community.ts
+++ types/community.ts
@@ -1,6 +1,8 @@
-export interface ICommunities {
+export interface ICommunity {
   name: string;
   owner_id: string;
   description: string;
   img: string;
+  isDeleted: boolean;
+  isActive: boolean;
 }
